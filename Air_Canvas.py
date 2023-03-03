fe20557632afe460e9e7e0bef9762c82db707f91
Abhi Kakadiya <abhikakadiya8477@gmail.com>
2023-03-04 01:17:03 +0530

    Add sound Effects to Drawing,Selecting Brush,eraseing

--- Air_Canvas.py
+++ Air_Canvas.py
@@ -5,7 +5,6 @@ import os
 import pygame
 import trackingModule as tm
 
-
 folderPath = 'Images'
 list_Images = os.listdir(folderPath)
 print(list_Images)
@@ -15,7 +14,11 @@ canvas_weight = 0.9
 
 X_Y_Points = []
 imagesArray = []
+
 pygame.mixer.init()
+drawingSound = pygame.mixer.Sound("Audio/drawingmp3.mp3")
+eraserSound = pygame.mixer.Sound("Audio/erasermp3.mp3")
+selectingSound = pygame.mixer.Sound("Audio/selectClickmp3.mp3")
 
 for imgPath in list_Images:
 	img = cv2.imread(f'{folderPath}/{imgPath}')
@@ -33,10 +36,10 @@ drawColor = (0, 0, 0)
 list1 = []
 xStart,yStart = 0,0
 
-x1Prev = 0
-y1Prev = 0
-x2Prev = 0
-y2Prev = 0
+# x1Prev = 0
+# y1Prev = 0
+# x2Prev = 0
+# y2Prev = 0
 
 canvas = np.ones((480,640,3),np.uint8)
 canvas[:,:] = canvas[:,:] * 255
@@ -45,8 +48,33 @@ displayImage = imagesArray[0]
 displayImage = cv2.resize(displayImage, (640, 60))
 
 handTracker = tm.handDetector()
- 
+
+soundStarted_drawing = False
+soundStartTime_drawing = 0
+soundDuration_drawing = 0
+
+soundStarted_eraser = False
+soundStartTime_eraser = 0
+soundDuration_eraser = 0
+
+soundStarted_selection = False
+soundStartTime_selection = 0
+soundDuration_selection = 0
+dict = {1:1,2:1,3:1,4:1,5:1}
+
 while True:
+
+	if not soundStarted_drawing:
+		soundDuration_drawing = drawingSound.get_length()
+		soundStarted_drawing = True
+
+	if not soundStarted_eraser:
+		soundDuration_eraser = eraserSound.get_length()
+		soundStarted_eraser = True
+	
+	if not soundStarted_selection:
+		soundDuration_selection = selectingSound.get_length()
+		soundStarted_selection = True
 
 	# IMAGE IMPORTED AND RESIZED
 	_,img = cap.read()
@@ -73,26 +101,101 @@ while True:
 
 		# SELECTION MODE
 		if (total_Fingers[1] ==True) and (total_Fingers[2] == True):
+
+			drawingSound.stop()
+			eraserSound.stop()
+
 			X_Y_Points = []
 			xStart = x1
 			yStart = y1
 			# SELECT DIFFERENT CRAYONS
 			if y2 < 60:
+				
 				if 200< ((x1+x2)//2) <288:
 					displayImage = imagesArray[1]
 					drawColor = (32, 66, 235)
+
+					timeElapsed = time.time() - soundStartTime_selection
+					soundStartTime_selection = time.time()
+					if int(timeElapsed) != 0 and dict[1]:
+						selectingSound.play()
+					else:
+						dict[1] = 0
+						dict[2] = 1
+						dict[3] = 1
+						dict[4] = 1
+						dict[5] = 1
+						soundStartTime_selection = 0
+					
+
 				elif 288< ((x1+x2)//2) <376:
 					displayImage = imagesArray[4]
 					drawColor = (255, 102, 196)
+
+					timeElapsed = time.time() - soundStartTime_selection
+					soundStartTime_selection = time.time()
+					if int(timeElapsed) != 0 and dict[2]:
+						selectingSound.play()
+					else:
+						dict[1] = 1
+						dict[2] = 0
+						dict[3] = 1
+						dict[4] = 1
+						dict[5] = 1
+						soundStartTime_selection = 0
+					
+
+
 				elif 376< ((x1+x2)//2) <464:
 					displayImage = imagesArray[3]
 					drawColor = (126, 217, 87)
+
+					timeElapsed = time.time() - soundStartTime_selection
+					soundStartTime_selection = time.time()
+					if int(timeElapsed) != 0 and dict[3]:
+						selectingSound.play()
+					else:
+						dict[1] = 1
+						dict[2] = 1
+						dict[3] = 0
+						dict[4] = 1
+						dict[5] = 1
+						soundStartTime_selection = 0
+					
+
 				elif 464< ((x1+x2)//2) <552:
 					displayImage = imagesArray[5]
 					drawColor = (255, 222, 89)
+
+					timeElapsed = time.time() - soundStartTime_selection
+					soundStartTime_selection = time.time()
+					if int(timeElapsed) != 0 and dict[4]:
+						selectingSound.play()
+					else:
+						dict[1] = 1
+						dict[2] = 1
+						dict[3] = 1
+						dict[4] = 0
+						dict[5] = 1
+						soundStartTime_selection = 0
+					
+
 				elif 552< ((x1+x2)//2) <640:
 					displayImage = imagesArray[2]
 					drawColor = (255,255,255)
+
+					timeElapsed = time.time() - soundStartTime_selection
+					soundStartTime_selection = time.time()
+					if int(timeElapsed) != 0 and dict[5]:
+						selectingSound.play()
+					else:
+						dict[1] = 1
+						dict[2] = 1
+						dict[3] = 1
+						dict[4] = 1
+						dict[5] = 0
+						soundStartTime_selection = 0
+
 			else:
 				displayImage = imagesArray[0]
 
@@ -100,12 +203,12 @@ while True:
 
 			cv2.circle(img,radius=20,center=((x1+x2)//2,(y1+y2)//2),color=drawColor,thickness=-1)
 
-			# print('SELECTION MODE')
+			print('SELECTION MODE')
 
 		# DRAWING MODE
-		elif ((total_Fingers[1] == True) and (total_Fingers[2] == False) and (total_Fingers[3] == False) and (total_Fingers[4] == False)) or (total_Fingers[3] == False):
-
-			sound = pygame.mixer.Sound("mixkit-writing-pencil-3194.wav")
+		elif ((total_Fingers[1] == True) and (total_Fingers[2] == False) and (total_Fingers[3] == False) and (total_Fingers[4] == False)) or (total_Fingers[3] == False):			
+		
+			selectingSound.stop()
 
 			if len(X_Y_Points) >= 0:
 				if len(X_Y_Points) == 0:
@@ -118,7 +221,7 @@ while True:
 					# print(X_Y_Points)
 					X_Y_Points.append([x1,y1])
 			
-			cv2.circle(img,radius=20,center=(x1,y1),color=drawColor,thickness=-1)		
+			cv2.circle(img,radius=20,center=(x1,y1),color=(0,0,0),thickness=-1)		
 			
 			# BY DEFAULT IT WILL NOT START DRAWING AT THE STARTING INDEX OF X AND Y --> (0,0)
 			if xStart == 0 and yStart == 0:
@@ -129,29 +232,55 @@ while True:
 			if drawColor == (255,255,255):
 				thickness = 40
 				cv2.line(canvas,(xStart,yStart),(x1,y1),color=drawColor,thickness=thickness)
+
+				if not pygame.mixer.get_busy():
+					soundStartTime_eraser = time.time()
+					eraserSound.play()
+				else:
+					timeElapsed = time.time() - soundStartTime_eraser
+					if timeElapsed > soundDuration_eraser:
+						eraserSound.stop()
+						eraserSound.play()
+						soundStartTime_eraser = time.time()
+
 			
 			elif drawColor != (0, 0, 0):
 				# cv2.line(canvas,(xStart,yStart),(x1,y1),color=drawColor,thickness=10)
-				points = np.array(X_Y_Points)				
-				cv2.polylines(canvas, [points], False, drawColor, thickness = 7)
-				# sound.play()
+				points = np.array(X_Y_Points)			
+				cv2.polylines(canvas, [points], False, drawColor, thickness = 7)	
+			
+				if not pygame.mixer.get_busy():
+					soundStartTime_drawing = time.time()
+					drawingSound.play()
+				else:
+					timeElapsed = time.time() - soundStartTime_drawing
+					if timeElapsed > soundDuration_drawing:
+						drawingSound.stop()
+						drawingSound.play()
+						soundStartTime_drawing = time.time()
 		
 			print('DRAWING MODE')
-			
+
 			# NOW OUR NEW POINTS WILL PREVIOUS POINTS
 			xStart = x1
 			yStart = y1
 		
 		displayImage = cv2.resize(displayImage, (640, 60))
+	
+	else:
+		eraserSound.stop()
+		drawingSound.stop()
+		selectingSound.stop()
 
 	# FIRST IMAGE TO BE DISPLAYED IS APPLIED
 	img[:60,0:640] = displayImage
-	blended = cv2.addWeighted(img[60:,0:640], img_weight, canvas[60:,0:640], canvas_weight, 0)
+	blended = cv2.addWeighted(img[60:,0:640],img_weight, canvas[60:,0:640], canvas_weight, 0)
 	img[60:,0:640] = blended
 	cv2.imshow('Canvas', img)
 
 	if cv2.waitKey(1) & 0xFF == 27:
 		break
 
+pygame.mixer.music.stop()
 cv2.destroyAllWindows()
 pygame.quit()
--- trackingModule.py
+++ trackingModule.py
@@ -4,102 +4,95 @@ import time
 
 
 class handDetector:
-	def __init__(self):
+    def __init__(self):
 
-		self.mpHands = mp.solutions.hands
-		self.mpDraw = mp.solutions.drawing_utils
+        self.mpHands = mp.solutions.hands
+        self.mpDraw = mp.solutions.drawing_utils
 
-		self.hand = self.mpHands.Hands(False,max_num_hands=2,min_tracking_confidence=0.5,min_detection_confidence=0.85)
+        self.hand = self.mpHands.Hands(
+            False, max_num_hands=2, min_tracking_confidence=0.5, min_detection_confidence=0.85)
 
+    def HandDetection(self, img, draw=True):
+        """ TO DETECT HAND IN WINDOW AND DRAW LANDMARKS ON IT """
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hand.process(imgRGB)
 
+        if self.result.multi_hand_landmarks:
+            for handlandmarks in self.result.multi_hand_landmarks:
+                if draw == True:
+                    self.mpDraw.draw_landmarks(
+                        img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-	def HandDetection(self, img, draw=True):
+    def findPosition(self, img, handNumber=0, draw=True):
+        """ TO STORE POSITION OF SELECTED HAND'S LANDMARK AND RETURN IT"""
+        self.landMarkList = []
+        if self.result.multi_hand_landmarks:
+            self.selectedHand = self.result.multi_hand_landmarks[handNumber]
+            for id, landmark in enumerate(self.selectedHand.landmark):
+                if draw:
+                    # print(id,landmark)
+                    height, width, channels = img.shape
+                    cX, cY = int(landmark.x * width), int(landmark.y * height)
 
-		""" TO DETECT HAND IN WINDOW AND DRAW LANDMARKS ON IT """
-		imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-		self.result = self.hand.process(imgRGB)
+                    # print(id,cX,cY)
+                    self.landMarkList.append([id, cX, cY])
 
-		if self.result.multi_hand_landmarks:
-			for handlandmarks in self.result.multi_hand_landmarks:
-				if draw == True:
-					self.mpDraw.draw_landmarks(img,handlandmarks,self.mpHands.HAND_CONNECTIONS)
-		return img
+        return self.landMarkList
 
+    def fingers_Fisted_Splayed(self):
 
-	def findPosition(self, img, handNumber=0,draw = True):
+        self.tip_IDX = [4, 8, 12, 16, 20]
+        self.fingers = []
 
-		""" TO STORE POSITION OF SELECTED HAND'S LANDMARK AND RETURN IT"""
-		self.landMarkList = [] 		
-		if self.result.multi_hand_landmarks:
-			self.selectedHand = self.result.multi_hand_landmarks[handNumber]
-			for id,landmark in enumerate(self.selectedHand.landmark):
-				if draw:
-					# print(id,landmark)
-					height,width,channels = img.shape
-					cX,cY = int(landmark.x * width),int(landmark.y * height)
+        if len(self.landMarkList) != 0:
 
-					# print(id,cX,cY)
-					self.landMarkList.append([id,cX,cY])
-		
-		return self.landMarkList
+            if self.landMarkList[self.tip_IDX[0]][1] > self.landMarkList[self.tip_IDX[0]-1][1]:
+                self.fingers.append(0)
+            else:
+                self.fingers.append(1)
 
-	def fingers_Fisted_Splayed(self):
-		
-		self.tip_IDX = [4,8,12,16,20]
-		self.fingers = []
+            for i in range(1, 5):
 
-		if len(self.landMarkList) != 0:
+                if (self.landMarkList[self.tip_IDX[i]][2] > self.landMarkList[self.tip_IDX[i]-1][2]):
+                    self.fingers.append(0)
+                else:
+                    self.fingers.append(1)
 
-			if self.landMarkList[self.tip_IDX[0]][1] > self.landMarkList[self.tip_IDX[0]-1][1] :
-				self.fingers.append(0)
-			else:
-				self.fingers.append(1)
+        self.fingersCount = self.fingers.count(1)
 
-
-			for i in range(1,5):
-					                       
-				if (self.landMarkList[self.tip_IDX[i]][2] > self.landMarkList[self.tip_IDX[i]-1][2]):
-					self.fingers.append(0)
-				else:
-					self.fingers.append(1)
-				
-		self.fingersCount = self.fingers.count(1)
-		
-		return list(self.fingers)
-					
+        return list(self.fingers)
 
 
 def main():
 
-	currentTime = 0
-	previousTime = 0
+    currentTime = 0
+    previousTime = 0
 
-	cap = cv2.VideoCapture(0)
-	detector = handDetector()
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
 
-	cap.set(cv2.CAP_PROP_FPS, 60)
+    cap.set(cv2.CAP_PROP_FPS, 120)
+    while True:
+        _, img = cap.read()
+        img = detector.HandDetection(img)
+        img = cv2.flip(img, 1)
+        detector.findPosition(img)
 
-	while True:
-		_,img = cap.read()
-		img = detector.HandDetection(img)
-		img = cv2.flip(img,1)
-		detector.findPosition(img)
-		
+        currentTime = time.time()
+        fps = 1/(currentTime-previousTime)
+        previousTime = currentTime
 
-		currentTime = time.time()
-		fps = 1/(currentTime-previousTime)
-		previousTime = currentTime
+        font = cv2.FONT_HERSHEY_COMPLEX
+        cv2.putText(img, text=str(int(fps)), org=(10, 70),
+                    fontFace=font, fontScale=2, color=(255, 0, 0), thickness=3)
+        cv2.imshow("Tracking", img)
 
-		font = cv2.FONT_HERSHEY_COMPLEX
-		cv2.putText(img,text=str(int(fps)),org=(10,70),fontFace=font,fontScale=2,color=(255,0,0),thickness=3)
-		cv2.imshow("Tracking",img)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
-		if cv2.waitKey(1) & 0xFF == 27:
-			break
-
-	cv2.destroyAllWindows()
-
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-	main()
+    main()
